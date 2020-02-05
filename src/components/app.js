25f614cd75c984049fef37f4eb0b8a8c19d63d7d
jackkk94 <jackpanchenko@mail.ru>
2020-02-05 21:33:20 +0300

    Выполнение задания 4

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React, {Component} from 'react';
+import WelcomeScreen from './welcome-screen';
+
+class App extends Component {
+  render() {
+    return (
+      <WelcomeScreen name="Jack" />
+    );
+  }
+}
+
+export default App;
